bd95a1d79b7848326f5c8e124330fdace359f2ef
pshho <awdr498@gmail.com>
2024-01-18 10:06:58 +0900

    Banking, BankingReuslt

--- hacking02_sk/src/main/java/org/example/hacking02_sk/model/Banking.java
+++ hacking02_sk/src/main/java/org/example/hacking02_sk/model/Banking.java
@@ -1,5 +1,7 @@
 package org.example.hacking02_sk.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import lombok.Data;
 import org.apache.ibatis.type.Alias;
 
@@ -8,4 +10,10 @@ import org.apache.ibatis.type.Alias;
 public class Banking {
     int myacc, mymoney, myaccpw;
     String myid, mybank;
+    Date myaccregdate;
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public String getMyaccregdate() {
+        return sdf.format(myaccregdate);
+    }
 }
