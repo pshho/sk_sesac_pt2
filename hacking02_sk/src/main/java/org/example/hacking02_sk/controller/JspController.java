daa8df3bd9b6ab2605f2a964a14e9cc2d37cdb5d
pshho <awdr498@gmail.com>
2024-01-20 20:34:24 +0900

    JSP Header, Footer, Bug Fix, Log4j2

--- hacking02_sk/src/main/java/org/example/hacking02_sk/controller/JspController.java
+++ hacking02_sk/src/main/java/org/example/hacking02_sk/controller/JspController.java
@@ -1,12 +1,15 @@
 package org.example.hacking02_sk.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JspController {
-    @GetMapping("jspFile")
-    String goJSP(){
-        return "jsp/a";
+    @RequestMapping("jsp/{path}")
+    String path(@PathVariable String path, HttpServletRequest request, HttpServletResponse response) {
+        return "jsp/" + path;
     }
 }
