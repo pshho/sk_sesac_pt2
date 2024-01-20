9928512eecddd5bed7fbca3d445eff4ffe076722
Hwiyong Chang <enghc95@gmail.com>
2024-01-20 13:57:08 +0900

    Update auto_build.sh for automcat:8.0.53

--- hacking02_sk/auto_build.sh
+++ hacking02_sk/auto_build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+webwas_container_name=$1
+
+if [ -z "$webwas_container_name" ]; then
+    echo "오류: 도커 컨테이너 이름을 지정해주세요."
+    echo "사용법: $0 <도커_컨테이너_이름>"
+    exit 1
+fi
+
 gradle build
-cp -f ./build/libs/hacking02_sk-0.0.1-SNAPSHOT.war /usr/local/tomcat/apache-tomcat-8.0.53/webapps/ROOT.war
+sesac_tomcat_path=/usr/local/tomcat
+sudo docker cp ./build/libs/hacking02_sk-*-SNAPSHOT.war ${webwas_container_name}:${sesac_tomcat_path}/webapps/ROOT.war
