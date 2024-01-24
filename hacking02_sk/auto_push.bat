cd08160f3fac773ea26d2d23ced3883ec81fedbb
pshho <awdr498@gmail.com>
2024-01-24 11:25:38 +0900

    user fix

--- hacking02_sk/auto_push.bat
+++ hacking02_sk/auto_push.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
 
 SET "ORI=D:\sk_shieldus\hacking02_sk\sk_sesac_pt2\hacking02_sk"
-FOR /F %%P IN ('cd') DO SET PATH=%%P
-IF "%ORI%" == "%PATH%" (
+FOR /F %%P IN ('cd') DO SET CURRENT_PATH=%%P
+IF "%ORI%" == "%CURRENT_PATH%" (
+	FOR /F "tokens=*" %%G IN ('gradlew build') DO SET MY_GRBUILD=%%G
+	ECHO "%MY_GRBUILD%"
 	
-	ECHO SUCCESS
+	:::FOR /F %%R IN ('(dir /B /A:-D) ^| findstr /I "ROOT.war"') DO SET MY_LIST=%%R
+	:::ECHO %MY_LIST%
 )
