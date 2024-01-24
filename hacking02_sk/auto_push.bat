d4e3e0925e3a82de23d21d3eee27523d33740bd2
pshho <awdr498@gmail.com>
2024-01-24 12:38:56 +0900

    complete_auto_push

--- hacking02_sk/auto_push.bat
+++ hacking02_sk/auto_push.bat
@@ -1,21 +1,22 @@
 @ECHO OFF
 
+SET commit_message=%1
 SET "ORI=D:\sk_shieldus\hacking02_sk\sk_sesac_pt2\hacking02_sk"
 FOR /F %%P IN ('cd') DO SET CURRENT_PATH=%%P
 IF "%ORI%" == "%CURRENT_PATH%" (
-	ECHO PATH SAME
-	SET "MY_BUILD="
-	FOR /F "tokens=*" %%G IN ('gradlew build ^| find /I "BUILD SUCCESSFUL"') DO SET "MY_BUILD=%%G"
+	 ECHO PATH SAME
+	 SET "MY_BUILD="
+	 FOR /F "tokens=*" %%G IN ('gradlew build ^| find /I "BUILD SUCCESSFUL"') DO SET "MY_BUILD=%%G"
 	
-	IF %ERRORLEVEL% EQU 0 (
-		ECHO BUILD SUCCESS
-		call COPY build\libs\hacking02_sk-0.0.1-SNAPSHOT-plain.war "%CD%\ROOT.war"
+	 IF %ERRORLEVEL% EQU 0 (
+		 ECHO BUILD SUCCESS
+		 call COPY build\libs\hacking02_sk-0.0.1-SNAPSHOT-plain.war "%CD%\ROOT.war"
 
-		FOR /F "tokens=*" %%C IN ('git branch ^| find /I "* psh240124"') DO SET "MY_GIT=%%C"
+		 FOR /F "tokens=*" %%C IN ('git branch ^| find /I "* psh240124"') DO SET "MY_GIT=%%C"
 		
-		IF %ERRORLEVEL% EQU 0 (
+		 IF %ERRORLEVEL% EQU 0 (
 			call git add .
-			call git commit -m "update"
+			call git commit -m "%commit_message%"
 			call git checkout master
 			call git merge psh240124
 			call git push origin master
